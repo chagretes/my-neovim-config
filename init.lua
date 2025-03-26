603611b8f2e1eb8d4bfe4700f897b9882344e99c
Felipe Chagas <felipe@chagas.top>
2025-03-25 21:37:41 -0300

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("chagretes")
+print("hello :D")
---
+++ lua/chagretes/init.lua
@@ -0,0 +1,7 @@
+require("chagretes.remap")
+require("chagretes.lazy")
+print("Hellow from chagretes")
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.OmniSharp_server_use_mono = 1
