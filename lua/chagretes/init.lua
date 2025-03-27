ab92639da1eca3901cb42faa49843f7f1fab3ba5
Felipe Chagas <felipe.chagas@wildlifestudios.com>
2025-03-27 08:52:27 -0300

    insensitive searching

--- lua/chagretes/init.lua
+++ lua/chagretes/init.lua
@@ -5,3 +5,6 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.OmniSharp_server_use_mono = 1
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
