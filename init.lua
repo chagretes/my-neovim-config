ba54d204b0f25f6503dce8f8de07857190d0b1fc
Felipe Chagas <felipe@chagas.top>
2026-03-06 13:13:10 -0300

    nvim update'

--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
 require("chagretes")
-print("hello :D")
--- lua/chagretes/init.lua
+++ lua/chagretes/init.lua
@@ -1,6 +1,5 @@
 require("chagretes.remap")
 require("chagretes.lazy")
-print("Hellow from chagretes")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
